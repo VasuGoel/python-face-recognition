6446db5612bc384ed0c4a0893a20bf4a121023ce
Vasu Goel <goelvasu0712@gmail.com>
2019-07-02 16:06:16 +0530

    face_recognition.face_locations(image)

--- findfaces.py
+++ findfaces.py
@@ -1,4 +1,15 @@
 import face_recognition
 
 # Load image as <class 'numpy.ndarray'>
-image = face_recognition.load_image_file('./img/groups/team1.jpg')
+image1 = face_recognition.load_image_file('./img/groups/team1.jpg')
+face_locations1 = face_recognition.face_locations(image1)
+
+image2 = face_recognition.load_image_file('./img/groups/team2.jpg')
+face_locations2 = face_recognition.face_locations(image2)
+
+# Array of coordinates (top, right, bottom, left) of each face
+print(f'Coordinates of the face locations in team1.jpg - \n{face_locations1}')
+print(f'There are {len(face_locations1)} people in team1.jpg\n')
+
+print(f'Coordinates of the face locations in team1.jpg - \n{face_locations2}')
+print(f'There are {len(face_locations2)} people in team1.jpg')
