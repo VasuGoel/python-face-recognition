da8228a3eb94fd190a258704a4dc230c3528aec8
Vasu Goel <goelvasu0712@gmail.com>
2019-07-02 16:25:24 +0530

    face_recognition.compare_faces([])

--- facematch.py
+++ facematch.py
@@ -3,10 +3,13 @@ import face_recognition
 known_image = face_recognition.load_image_file('./img/known/Bill Gates.jpg')
 known_face_encoding = face_recognition.face_encodings(known_image)[0]
 
-unknown_image = face_recognition.load_image_file('./img/unknown/obama.jpeg')
+unknown_image = face_recognition.load_image_file('./img/unknown/bill-gates-4.jpg')
 unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
 # Compare faces
 result = face_recognition.compare_faces([known_face_encoding], unknown_face_encoding)
 
-print(result)
+if result[0]:
+    print('Known and unknown images are SAME')
+else:
+    print('Known and unknown images are NOT SAME')
