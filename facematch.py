ee7774258a5e5dc90b31917eded8a5c524d3ebdc
Vasu Goel <goelvasu0712@gmail.com>
2019-07-02 16:20:45 +0530

    Comapre face encodings

--- facematch.py
+++ facematch.py
@@ -1,7 +1,12 @@
 import face_recognition
 
 known_image = face_recognition.load_image_file('./img/known/Bill Gates.jpg')
-known_face_encoding = face_recognition.face_encodings(known_image)
+known_face_encoding = face_recognition.face_encodings(known_image)[0]
 
 unknown_image = face_recognition.load_image_file('./img/unknown/obama.jpeg')
-unknown_face_encoding = face_recognition.face_encodings(unknown_image)
+unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+# Compare faces
+result = face_recognition.compare_faces([known_face_encoding], unknown_face_encoding)
+
+print(result)
