cb64bd6ab3a8abeab355778a44b8247e59e586a3
Vasu Goel <goelvasu0712@gmail.com>
2019-07-04 15:43:01 +0530

    Create Bill Gates and Steve Jobs face encodings

--- identify.py
+++ identify.py
@@ -1,2 +1,8 @@
 import face_recognition
 from PIL import Image, ImageDraw
+
+image_of_bill = face_recognition.load_image_file('img/known/Bill Gates.jpg')
+bill_faec_encoding = face_recognition.face_encodings(image_of_bill)[0]
+
+image_of_steve = face_recognition.load_image_file('img/known/Steve Jobs.png')
+steve_face_encoding = face_recognition.face_encodings(image_of_steve)[0]
