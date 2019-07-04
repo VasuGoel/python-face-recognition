fe0dd784ff04e58c9f4e1e62cbc94199082243b3
Vasu Goel <goelvasu0712@gmail.com>
2019-07-04 15:44:41 +0530

    Create array of encodings and names of known people

--- identify.py
+++ identify.py
@@ -2,7 +2,18 @@ import face_recognition
 from PIL import Image, ImageDraw
 
 image_of_bill = face_recognition.load_image_file('img/known/Bill Gates.jpg')
-bill_faec_encoding = face_recognition.face_encodings(image_of_bill)[0]
+bill_face_encoding = face_recognition.face_encodings(image_of_bill)[0]
 
 image_of_steve = face_recognition.load_image_file('img/known/Steve Jobs.png')
 steve_face_encoding = face_recognition.face_encodings(image_of_steve)[0]
+
+# Create array of encodings and names
+known_face_encodings = [
+    bill_face_encoding,
+    steve_face_encoding
+]
+
+known_face_names = [
+    "Bill Gates",
+    "Steve Jobs"
+]
